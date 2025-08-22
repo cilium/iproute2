75e4587a4b94268df0ebe18bbec5150dd890103f
Daniel Borkmann <daniel@iogearbox.net>
2025-08-22 13:32:34 +0000

    Revert "sync kernel uapi header"
    This reverts commit 4cb2ad5039970bc14e434a2eec4afa2860155983.

--- include/uapi/linux/if_link.h
+++ include/uapi/linux/if_link.h
@@ -1305,12 +1305,6 @@ enum netkit_scrub {
 	NETKIT_SCRUB_DEFAULT,
 };
 
-struct netkit_lowerdev {
-	__u32 ifindex;
-	__u32 queue_id_from;
-	__u32 queue_id_to;
-};
-
 enum {
 	IFLA_NETKIT_UNSPEC,
 	IFLA_NETKIT_PEER_INFO,
@@ -1322,7 +1316,6 @@ enum {
 	IFLA_NETKIT_PEER_SCRUB,
 	IFLA_NETKIT_HEADROOM,
 	IFLA_NETKIT_TAILROOM,
-	IFLA_NETKIT_LOWERDEV,
 	__IFLA_NETKIT_MAX,
 };
 #define IFLA_NETKIT_MAX	(__IFLA_NETKIT_MAX - 1)
@@ -1990,20 +1983,5 @@ enum {
 };
 
 #define IFLA_DSA_MAX	(__IFLA_DSA_MAX - 1)
-
-/* OVPN section */
-
-enum ovpn_mode {
-	OVPN_MODE_P2P,
-	OVPN_MODE_MP,
-};
-
-enum {
-	IFLA_OVPN_UNSPEC,
-	IFLA_OVPN_MODE,
-	__IFLA_OVPN_MAX,
-};
-
-#define IFLA_OVPN_MAX	(__IFLA_OVPN_MAX - 1)
 
 #endif /* _LINUX_IF_LINK_H */
